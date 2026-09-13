8b2efb5c0cccbd03afc52e89438c915e7be2a523
taj299 <shaikhtajoddin299@gmail.com>
2026-09-13 21:33:46 +0530

    Add admin panel

---
+++ public/script.js
@@ -0,0 +1,13 @@
+console.log("Taj Portfolio Admin Panel loaded");
+
+const logoutButton = document.querySelector(".logout-btn");
+
+logoutButton.addEventListener("click", function () {
+    alert("Logout functionality will be added later.");
+});
+
+const addProjectButton = document.querySelector(".add-btn");
+
+addProjectButton.addEventListener("click", function () {
+    alert("Project management will be added in the next step.");
+});
